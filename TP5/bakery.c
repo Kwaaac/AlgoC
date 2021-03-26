44b9c1e3fdbc5c638d4c5862c64759905e66435d
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2021-03-26 08:55:28 +0100

    Last Maj Exercice 7

--- TP5/bakery.c
+++ TP5/bakery.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
 #include "event.h"
-#include "customer.h"
 #include "queue.h"
 #include "prioqueue.h"
 
+
 #define N_VENDORS 3
-#define VENDOR_TIME 60
-#define CLOSING_TIME 420
+#define CLOSING_TIME 360
+#define ARRIVAL_RATE (1.0/60)
+#define MEAN_SERVICE_TIME 150
 
 prioqueue *event_queue;
 queue *customer_queue;
@@ -32,11 +37,18 @@ void print_simulation() {
     }
 }
 
+double normal_delay(double mean) {
+    return -mean * log(1 - ((double) rand() / RAND_MAX));
+}
+
 void add_customer(customer *c) {
     int i;
+    event *event;
     for (i = 0; i < N_VENDORS; i++) {
         if (vendor[i] == NULL) {
             vendor[i] = c;
+            event = create_departure((int) (current_time + normal_delay(MEAN_SERVICE_TIME)), c);
+            insert_pq(event_queue, event);
             return;
         }
     }
@@ -52,6 +64,7 @@ void remove_customer(customer *c) {
     for (i = 0; i < N_VENDORS; i++) {
         if (vendor[i] == c) {
             vendor[i] = NULL;
+            free_customer(c);
             break;
         }
     }
@@ -59,20 +72,21 @@ void remove_customer(customer *c) {
     if (size_q(customer_queue)) {
         c2 = dequeue_q(customer_queue);
         vendor[i] = c2;
-        event = create_departure(current_time + 150, c2);
+        event = create_departure((int) (current_time + normal_delay(MEAN_SERVICE_TIME)), c2);
         insert_pq(event_queue, event);
     }
-
-    free_customer(c);
 }
 
 void process_arrival(event *e) {
     int time;
+    customer *customer;
+    event *event;
     add_customer(e->c);
 
-    time = current_time + 60;
-    e->c->atime = current_time + 60;
-    event *event = create_arrival(e->c->atime, e->c);
+    time = (int) (current_time + normal_delay(1.0/ARRIVAL_RATE));
+    customer = create_customer(time);
+
+    event = create_arrival(time, customer);
     insert_pq(event_queue, event);
 }
 
@@ -80,11 +94,13 @@ void process_departure(event *e) {
     remove_customer(e->c);
 }
 
-
 int main() {
     int i;
+    customer *customer;
+    event *event_arrival;
 
     /* Init */
+    srand( time( NULL ) );
     event_queue = create_pq();
     customer_queue = create_q();
     for (i = 0; i < N_VENDORS; i++) {
@@ -92,9 +108,9 @@ int main() {
     }
     current_time = 0;
 
-    customer *customer = create_customer(50);
+    customer = create_customer(50);
 
-    event *event_arrival = create_arrival(60, customer);
+    event_arrival = create_arrival(60, customer);
     insert_pq(event_queue, event_arrival);
 
     while (size_pq(event_queue) && current_time < CLOSING_TIME) {
